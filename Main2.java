75bdef21f0b3cef3f95928f72d887ddf421a9589
106021378 <32055897+106021378@users.noreply.github.com>
2017-09-25 21:24:26 +0800

    20170925

---
+++ Main2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main2 {
+public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int v1 = scn.nextInt();
+    System.out.println(Math.round((v1/600f)*10/10f));
+}
+}
